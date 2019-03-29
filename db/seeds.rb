123fd78c604d2d40dfecbe8e3a0c740d555083d0
Rhohoman <rhohoman@gmail.com>
2019-03-29 11:38:28 -0400

    added some stuff to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,29 +20,29 @@ statues = Team.create!(location: "Staten Island",name: "Staten Island Statues", 
 flatironers = Team.create!(location: "Brooklyn",name: "Brooklyn Flatironers", mascot: "Flatiron Building")
 
 
-kobe = Player.create!(name:"Kobe Bryant", position: "Shooting Guard", team_id: bobcats.id, age: 28, img_url: "https://i.pinimg.com/474x/34/77/c3/3477c3b54457ef50c2e03bdaa7b3fdc5.jpg")
-lebron = Player.create!(name:"Lebron James", position: "Shooting Guard", team_id: herbs.id, age: 28, img_url: "https://i.pinimg.com/474x/34/77/c3/3477c3b54457ef50c2e03bdaa7b3fdc5.jpg")
-spongebob = Player.create!(name:"SpongeBob Squarepants", position: "Shooting Guard", team_id: statues.id, age: 28, img_url: "https://i.pinimg.com/474x/34/77/c3/3477c3b54457ef50c2e03bdaa7b3fdc5.jpg")
-john = Player.create!(name:"John Starks", position: "Shooting Guard", team_id: flatironers.id, age: 28, img_url: "https://i.pinimg.com/474x/34/77/c3/3477c3b54457ef50c2e03bdaa7b3fdc5.jpg")
+kobe = Player.create!(name:"Kobe Bryant", position: "Shooting Guard", team_id: bobcats.id, age: 28, img_url: "https://media.giphy.com/media/xT9KVwfZ1xyA3y6qcw/giphy.gif")
+lebron = Player.create!(name:"Lebron James", position: "Shooting Guard", team_id: herbs.id, age: 28, img_url: "https://media.giphy.com/media/l0Iyh6zphUKTjaxoI/giphy.gif")
+spongebob = Player.create!(name:"SpongeBob Squarepants", position: "Shooting Guard", team_id: statues.id, age: 28, img_url: "https://media.giphy.com/media/M8xmO5ZcLPtAY/giphy.gif")
+john = Player.create!(name:"John Starks", position: "Shooting Guard", team_id: flatironers.id, age: 28, img_url: "https://media.giphy.com/media/tP1ZTDH8MYt0I/giphy.gif")
 
-ray = Player.create!(name:"Ray Allen", position: "Point Guard", team_id: bobcats.id, age: 28, img_url: "https://i.pinimg.com/474x/34/77/c3/3477c3b54457ef50c2e03bdaa7b3fdc5.jpg")
+ray = Player.create!(name:"Ray Allen", position: "Point Guard", team_id: bobcats.id, age: 28, img_url: "https://media.giphy.com/media/l0ErN4njtUxLwQNHi/giphy.gif")
 desk = Player.create!(name:"Woody Allen", position: "Point Guard", team_id: herbs.id, age: 28, img_url: "https://i.pinimg.com/474x/34/77/c3/3477c3b54457ef50c2e03bdaa7b3fdc5.jpg")
-will = Player.create!(name:"Will Mannychester", position: "Point Guard", team_id: statues.id, age: 28, img_url: "https://i.pinimg.com/474x/34/77/c3/3477c3b54457ef50c2e03bdaa7b3fdc5.jpg")
-jim = Player.create!(name:"James Washington IV", position: "Point Guard", team_id: flatironers.id, age: 28, img_url: "https://i.pinimg.com/474x/34/77/c3/3477c3b54457ef50c2e03bdaa7b3fdc5.jpg")
+will = Player.create!(name:"Will Mannychester", position: "Point Guard", team_id: statues.id, age: 28, img_url: "https://media.giphy.com/media/T2AmoAqrjAluU/giphy.gif")
+jim = Player.create!(name:"James Washington IV", position: "Point Guard", team_id: flatironers.id, age: 28, img_url: "https://raw.githubusercontent.com/peidansauyukjuk/deploy-on-day-1-dumbo-web-career-021819/master/img/students/james_dorr_profile.jpg")
 
-simone = Player.create!(name:"Simone May Antetekumpo", position: "Center", team_id: bobcats.id, age: 28, img_url: "https://i.pinimg.com/474x/34/77/c3/3477c3b54457ef50c2e03bdaa7b3fdc5.jpg")
-brain = Player.create!(name:"Brain Ury", position: "Center", team_id: herbs.id, age: 28, img_url: "https://i.pinimg.com/474x/34/77/c3/3477c3b54457ef50c2e03bdaa7b3fdc5.jpg")
-matt = Player.create!(name:"Matt Bye", position: "Center", team_id: statues.id, age: 28, img_url: "https://i.pinimg.com/474x/34/77/c3/3477c3b54457ef50c2e03bdaa7b3fdc5.jpg")
-fred = Player.create!(name:"Freddi Flintstone", position: "Center", team_id: flatironers.id, age: 28, img_url: "https://i.pinimg.com/474x/34/77/c3/3477c3b54457ef50c2e03bdaa7b3fdc5.jpg")
+simone = Player.create!(name:"Simone May Antetekumpo", position: "Center", team_id: bobcats.id, age: 28, img_url: "https://github.com/peidansauyukjuk/deploy-on-day-1-dumbo-web-career-021819/blob/master/img/students/simon_mei_profile.jpeg?raw=true")
+brain = Player.create!(name:"Brain Ury", position: "Center", team_id: herbs.id, age: 28, img_url: "https://raw.githubusercontent.com/peidansauyukjuk/deploy-on-day-1-dumbo-web-career-021819/master/img/students/brian_ryu.jpg")
+matt = Player.create!(name:"Matt Bye", position: "Center", team_id: statues.id, age: 28, img_url: "https://media.licdn.com/dms/image/C4D03AQFJXw540SrrzQ/profile-displayphoto-shrink_800_800/0?e=1559174400&v=beta&t=Aiy0Tlwz4ILYftfhG0fQVdYvRIzEwGEyku3sFfJijjY")
+fred = Player.create!(name:"Freddi Flintstone", position: "Center", team_id: flatironers.id, age: 28, img_url: "https://media.giphy.com/media/1it9hyynPEqZKrNo1Y/giphy.gif")
 
-jo = Player.create!(name:"Jordan Belford", position: "Power Forward", team_id: bobcats.id, age: 28, img_url: "https://i.pinimg.com/474x/34/77/c3/3477c3b54457ef50c2e03bdaa7b3fdc5.jpg")
-beef = Player.create!(name:"Cow Man", position: "Power Forward", team_id: herbs.id, age: 28, img_url: "https://i.pinimg.com/474x/34/77/c3/3477c3b54457ef50c2e03bdaa7b3fdc5.jpg")
-astro = Player.create!(name:"Tacko Falls", position: "Power Forward", team_id: statues.id, age: 28, img_url: "https://i.pinimg.com/474x/34/77/c3/3477c3b54457ef50c2e03bdaa7b3fdc5.jpg")
-zion = Player.create!(name:"Zion Superfragilistickexpialadoucious", position: "Power Forward", team_id: flatironers.id, age: 28, img_url: "https://i.pinimg.com/474x/34/77/c3/3477c3b54457ef50c2e03bdaa7b3fdc5.jpg")
+jo = Player.create!(name:"Jordan Belford", position: "Power Forward", team_id: bobcats.id, age: 28, img_url: "https://media.giphy.com/media/OTg4nrmU8vkty/giphy.gif")
+beef = Player.create!(name:"Cow Man", position: "Power Forward", team_id: herbs.id, age: 28, img_url: "https://media.giphy.com/media/h55EUEsTG9224/giphy.gif")
+astro = Player.create!(name:"Tacko Falls", position: "Power Forward", team_id: statues.id, age: 28, img_url: "https://media.giphy.com/media/9Pvfql9AWrRXCi2Oga/giphy.gif")
+zion = Player.create!(name:"Zion Superfragilistickexpialadoucious", position: "Power Forward", team_id: flatironers.id, age: 28, img_url: "https://media.giphy.com/media/BWCHh3nCM0qtRm5fyo/giphy.gif")
 
-doublelift = Player.create!(name:"Duuble Lift", position: "Small Forward", team_id: bobcats.id, age: 28, img_url: "https://i.pinimg.com/474x/34/77/c3/3477c3b54457ef50c2e03bdaa7b3fdc5.jpg")
-dan = Player.create!(name:"Dan White", position: "Small Forward", team_id: herbs.id, age: 28, img_url: "https://i.pinimg.com/474x/34/77/c3/3477c3b54457ef50c2e03bdaa7b3fdc5.jpg")
-pie = Player.create!(name:"Pie Guy", position: "Small Forward", team_id: statues.id, age: 28, img_url: "https://i.pinimg.com/474x/34/77/c3/3477c3b54457ef50c2e03bdaa7b3fdc5.jpg")
+doublelift = Player.create!(name:"Duuble Lift", position: "Small Forward", team_id: bobcats.id, age: 28, img_url: "https://media.giphy.com/media/SIlGZtFjpMhq/giphy.gif")
+dan = Player.create!(name:"Dan White", position: "Small Forward", team_id: herbs.id, age: 28, img_url: "https://media.giphy.com/media/mpYqfyPHoHnxu/giphy.gif")
+pie = Player.create!(name:"Pie Guy", position: "Small Forward", team_id: statues.id, age: 28, img_url: "https://media.giphy.com/media/thln8oJs58o7u/giphy.gif")
 basketball = Player.create!(name:"Soccer Man", position: "Small Forward", team_id: flatironers.id, age: 28, img_url: "https://i.pinimg.com/474x/34/77/c3/3477c3b54457ef50c2e03bdaa7b3fdc5.jpg")
 
 
