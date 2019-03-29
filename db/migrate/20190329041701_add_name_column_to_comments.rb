55c02f076201128c762eb839aed119c7f71c470c
cstruong <christophertruongm@gmail.com>
2019-03-29 09:32:10 -0400

    added profile pic and column to players and name column and name to comments as well as fixed delete team bug for stats

---
+++ db/migrate/20190329041701_add_name_column_to_comments.rb
@@ -0,0 +1,5 @@
+class AddNameColumnToComments < ActiveRecord::Migration[5.2]
+  def change
+    add_column :comments, :name, :string
+  end
+end
---
+++ db/migrate/20190329130305_add_img_url_column_to_players.rb
@@ -0,0 +1,5 @@
+class AddImgUrlColumnToPlayers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :players, :img_url, :string
+  end
+end
